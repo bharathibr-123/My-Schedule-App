d505537e5f48de1eb0f57495f581825ab615dec6
bharathibr-123 <bharathibrbharathi123@gmail.com>
2026-06-02 19:30:17 +0530

    Add files via upload

---
+++ sw[1].js
@@ -0,0 +1,4 @@
+const CACHE='my-schedule-v6';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.add('/').catch(()=>{})));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>{if(r)return r;return fetch(e.request).then(resp=>{if(resp&&resp.ok&&e.request.method==='GET'){const rc=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,rc));}return resp;}).catch(()=>caches.match('/index.html'));}));});
